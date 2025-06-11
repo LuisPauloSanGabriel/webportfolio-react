6d95aff5ec9d83647c34ed5de06401b827728131
luissangabriel <lp.sangabriel@gmail.com>
2025-06-11 22:51:25 +0800

    Applied smooth scroll and glassy navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,15 +5,15 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavigationBar() {
 
 	return (
-	<Navbar id="Nav" expand="lg">
+	<Navbar id="Nav" expand="lg" className="sticky-top shadow-sm">
       <Container>
         <Navbar.Brand href="#landing" className="nav-link-custom">Luis</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#landing" className="nav-link-custom nav-link-effect">Home</Nav.Link>
-            <Nav.Link href="#works" className="nav-link-custom nav-link-effect">Works</Nav.Link>
-            <Nav.Link href="#ToolsAndTechStack" className="nav-link-custom nav-link-effect">Tools</Nav.Link>
+            <Nav.Link href="#works" className="nav-link-custom nav-link-effect">Projects</Nav.Link>
+            <Nav.Link href="#ToolsAndTechStack" className="nav-link-custom nav-link-effect">TechStack</Nav.Link>
             <Nav.Link href="#contact" className="nav-link-custom nav-link-effect">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
