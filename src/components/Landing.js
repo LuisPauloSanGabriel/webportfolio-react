0273661877f0392403f89e3f31df12fa8233ed6a
luissangabriel <lp.sangabriel@gmail.com>
2025-06-11 23:22:30 +0800

    fixed flex direction of button and socmed icons in medium and large screens

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,7 +19,7 @@ export default function Landing() {
 				<h2 id="position" className="landing-info">IT Instructor & Aspiring Full-Stack Web Developer</h2>
 				<p>With a strong foundation in both education and technology, I bring a unique blend of real-world IT expertise and academic experience. As a licensed professional and lifelong learner, I’m passionate about building impactful digital solutions and guiding others on their tech journey...<a href="#"><i class="bi bi-arrow-right-circle ms-2"></i></a></p>
 				
-				<div id="socials" className="mt-5 d-flex flex-column  flex-md-row gap-3">
+				<div id="socials" className="mt-5 d-flex flex-column flex-md-column flex-lg-row gap-3">
 					<a className="custom-btn-primary text-white text-center shadow p-2 px-4 mx-auto mx-md-auto mx-lg-0 justify-content-center align-items-center hover-pop" href="https://drive.google.com/file/d/12OlXFmkGn9VGSpSi6aHDB8ePglE-Yu17/view?usp=sharing">Download CV<i className="bi bi-file-earmark-arrow-down ms-2"></i></a>
 					<div className="d-flex flex-row justify-content-center align-items-center ">
 						<a href="https://github.com/LuisPauloSanGabriel" target="_blank"><img className="socmed me-3 mb-3 hover-pop" src={github} alt="Github" /></a>
